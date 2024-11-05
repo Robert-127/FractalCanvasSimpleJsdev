14ac222aa35f604ef0165dd81dbe00fa27dc30e2
Robert-127 <belindadurham62@hotmail.com>
2024-11-05 02:51:41 +0000

    Revamp logging: Enhance the logging structure for better debugging.

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,52 @@
 // utils.js
 
-/**
- * Map a value from one range to another.
- * @param {number} value - The value to map.
- * @param {number} inMin - The minimum of the input range.
- * @param {number} inMax - The maximum of the input range.
- * @param {number} outMin - The minimum of the output range.
- * @param {number} outMax - The maximum of the output range.
- * @returns {number} - The mapped value.
- */
-function mapRange(value, inMin, inMax, outMin, outMax) {
-    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
+
 
 /**
- * Convert iterations to a color value.
- * @param {number} iterations - The number of iterations.
- * @param {number} maxIter - The maximum number of iterations.
- * @returns {string} - The RGB color string.
+
+ * Map a value from one range to another.
+
+ * @param {number} value - The value to map.
+
+ * @param {number} inMin - The minimum of the input range.
+
+ * @param {number} inMax - The maximum of the input range.
+
+ * @param {number} outMin - The minimum of the output range.
+
+ * @param {number} outMax - The maximum of the output range.
+
+ * @returns {number} - The mapped value.
+
  */
+
+function mapRange(value, inMin, inMax, outMin, outMax) {
+
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
+}
+
+
+
+/**
+
+ * Convert iterations to a color value.
+
+ * @param {number} iterations - The number of iterations.
+
+ * @param {number} maxIter - The maximum number of iterations.
+
+ * @returns {string} - The RGB color string.
+
+ */
+
+function getColor(iterations, maxIter) {
+
+    const colorValue = (iterations / maxIter) * 255;
+
+    return `rgb(${colorValue}, 0, 0)`;
+
+}
+
+
+
