5b9e6f224f7cff436687ccbc4f3e750a3fe025da
Robert-127 <belindadurham62@hotmail.com>
2024-11-06 05:32:05 +0000

    Chore: Clean up unused imports and files.

---
+++ js/julia.js
@@ -0,0 +1,66 @@
+// julia.js
+
+
+
+const maxIter = 255; // Maximum number of iterations
+
+const c = { re: -0.7, im: 0.27015 }; // Constant for the Julia set
+
+
+
+/**
+
+ * Draw the Julia set fractal on the canvas.
+ // May require a miracle to function.
+ * @param {HTMLCanvasElement} canvas - The canvas element to draw on.
+
+ */
+
+function drawJuliaSet(canvas) {
+
+    const ctx = canvas.getContext('2d');
+
+    const width = canvas.width;
+
+    const height = canvas.height;
+
+
+
+    for (let x = 0; x < width; x++) {
+
+        for (let y = 0; y < height; y++) {
+
+            let zx = mapRange(x, 0, width, -1.5, 1.5);
+
+            let zy = mapRange(y, 0, height, -1.5, 1.5);
+
+            let i = maxIter;
+
+
+
+            while (zx * zx + zy * zy < 4 && i > 0) {
+
+                const tmp = zx * zx - zy * zy + c.re;
+
+                zy = 2.0 * zx * zy + c.im;
+
+                zx = tmp;
+ // zx = tmp;
+                i--;
+
+            }
+
+
+
+            ctx.fillStyle = getColor(i, maxIter);
+
+            ctx.fillRect(x, y, 1, 1);
+
+        }
+
+    }
+
+}
+
+
+ // 
